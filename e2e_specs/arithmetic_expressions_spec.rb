f03d47150c0a5d5f703ebfda072ac620dc920b43
Riley Littlefield <rileylittlefield@ymail.com>
2017-11-18 17:23:54 -0500

    refactors e2e specs to minimize duplication

--- e2e_specs/arithmetic_expressions_spec.rb
+++ e2e_specs/arithmetic_expressions_spec.rb
@@ -5,18 +5,20 @@ require_relative '../src/interpreter.rb'
 
 describe 'rCalc' do
 
+  before do
+    @rcalc = RCalc.new(Interpreter.new)
+  end
+
   it 'evaluates the lone numbers' do
-    rcalc = RCalc.new(Interpreter.new)
-    rcalc.enter('5')
-    rcalc.submit
-    expect(rcalc.output).to eq('5')
+    @rcalc.enter('5')
+    @rcalc.submit
+    expect(@rcalc.output).to eq('5')
   end
 
   it 'evaluates expressions containing the addition operator' do
-    rcalc = RCalc.new(Interpreter.new)
-    rcalc.enter('1 + 1')
-    rcalc.submit
-    expect(rcalc.output).to eq('2')
+    @rcalc.enter('1 + 1')
+    @rcalc.submit
+    expect(@rcalc.output).to eq('2')
   end
 
 end
