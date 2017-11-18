f53c29d1935c2305b2b15293f368cd250feaea3d
Riley Littlefield <rileylittlefield@ymail.com>
2017-11-18 17:26:57 -0500

    refactors rcalc_spec to minimize duplicatino

--- unit_specs/rcalc_spec.rb
+++ unit_specs/rcalc_spec.rb
@@ -3,34 +3,33 @@ require_relative '../src/rcalc.rb'
 
 describe RCalc do
 
+  before do
+    @interpreter = spy('interpreter')
+    @rcalc = RCalc.new(@interpreter)
+  end
+
   describe '#initialize' do
 
   end
 
   describe '#enter' do
     it 'sets the value of input' do
-      interpreter = spy('interpreter')
-      rcalc = RCalc.new(interpreter)
-      rcalc.enter('8')
-      expect(rcalc.input).to eq('8')
+      @rcalc.enter('8')
+      expect(@rcalc.input).to eq('8')
     end
   end
 
   describe '#submit' do
     it 'computes the entered expression' do
-      interpreter = spy('interpreter')
-      rcalc = RCalc.new(interpreter)
-      rcalc.enter('8')
-      rcalc.submit
-      expect(rcalc.output).to eq('8')
+      @rcalc.enter('8')
+      @rcalc.submit
+      expect(@rcalc.output).to eq('8')
     end
 
     it "passes the input to the interpreter" do
-      interpreter = spy('interpreter')
-      rcalc = RCalc.new(interpreter)
-      rcalc.enter('8')
-      rcalc.submit
-      expect(interpreter).to have_received(:interpret).with('8')
+      @rcalc.enter('8')
+      @rcalc.submit
+      expect(@interpreter).to have_received(:interpret).with('8')
     end
   end
 
