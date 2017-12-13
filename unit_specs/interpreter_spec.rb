43a32146ba71fa2e0e96c490351f2ef68df617a9
Riley Littlefield <rileylittlefield@ymail.com>
2017-12-12 21:16:56 -0500

    adds unit spec for Interpreter#interpret

--- unit_specs/interpreter_spec.rb
+++ unit_specs/interpreter_spec.rb
@@ -4,26 +4,34 @@ require_relative '../src/calculator_input.rb'
 
 describe Interpreter do
 
+  before do
+    @interpreter = Interpreter.new
+  end
   describe '#interpret' do
 
     skip "converts string expression to an array of numbers" do
-      interpreter = Interpreter.new
-      expect(interpreter.interpret('3 + 2')).to eq([3, 2])
-      expect(interpreter.interpret('3+2')).to eq([3, 2])
-      expect(interpreter.interpret('    3 +2   ')).to eq([3, 2])
-      expect(interpreter.interpret('3')).to eq([3])
-      expect(interpreter.interpret('  3  ')).to eq([3])
+      expect(@interpreter.interpret('3 + 2')).to eq([3, 2])
+      expect(@interpreter.interpret('3+2')).to eq([3, 2])
+      expect(@interpreter.interpret('    3 +2   ')).to eq([3, 2])
+      expect(@interpreter.interpret('3')).to eq([3])
+      expect(@interpreter.interpret('  3  ')).to eq([3])
     end
 
     skip "interprets an expression with more than two numbers" do
-      interpreter = Interpreter.new
-      expect(interpreter.interpret('10 + 11 + 13')).to eq([10, 11, 13])
+      expect(@interpreter.interpret('10 + 11 + 13')).to eq([10, 11, 13])
     end
 
     it "transforms input into an instance of CalculatorInput" do
-      interpreter = Interpreter.new
-      calculator_input = interpreter.interpret('10 + 4 - 3')
+      calculator_input = @interpreter.interpret('10 + 4 - 3')
       expect(calculator_input).to be_a CalculatorInput
+    end
+
+    describe 'returned CalculatorInput' do
+
+      it "has initial value equal to the first value of the string input" do
+        calculator_input = @interpreter.interpret('10 + 4 - 3')
+        expect(calculator_input.initial_value).to eq(10)
+      end
     end
 
   end
