f7f98d5ab8fb259f11c9933495b54e98fe85b38d
Riley Littlefield <rileylittlefield@ymail.com>
2017-12-12 21:39:17 -0500

    adds inital value to calculator input returned from Interpreter#interpret

--- src/calculator_input.rb
+++ src/calculator_input.rb
@@ -1,2 +1,8 @@
 class CalculatorInput
+
+  attr_reader :initial_value
+
+  def initialize(state)
+    @initial_value = state[:initial_value]
+  end
 end
--- src/interpreter.rb
+++ src/interpreter.rb
@@ -3,7 +3,9 @@ require_relative 'calculator_input.rb'
 class Interpreter
 
   def interpret(input)
-    CalculatorInput.new
+    formatted_input = input.gsub(' ', '')
+    initial_value = formatted_input.split(/\+|-/).map(&:to_f)[0]
+    CalculatorInput.new(initial_value: initial_value)
   end
 
 end
