3fd976888f74dd8d6febace20aa3d163dd1354ee
Riley Littlefield <rileylittlefield@ymail.com>
2017-12-12 21:02:34 -0500

    adds unit spec to interpreter

---
+++ planning/Interper_Output_Planning.md
@@ -0,0 +1,20 @@
+# Supports Addition and Subtraction
+
+  1. return arrays of N number,
+      and an array (N - 1) operators
+
+  2. return non homogenous array of numbers and operators
+
+  3. return new data structure
+    Instance var for each arrays
+
+  4. initial value, (N - 1) pairs of operator & values, iterator
+
+  1 + 3 + 5
+
+  @initialVal = 1
+  @operatorValPairs = [{ operator: :+, val: 3 }, { operator: :+, val: 5 }]
+
+  #forEach
+
+  introduce class Operation
--- unit_specs/interpreter_spec.rb
+++ unit_specs/interpreter_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative '../src/interpreter.rb'
+require_relative '../src/calculator_input.rb'
 
 describe Interpreter do
 
@@ -17,6 +18,12 @@ describe Interpreter do
     skip "interprets an expression with more than two numbers" do
       interpreter = Interpreter.new
       expect(interpreter.interpret('10 + 11 + 13')).to eq([10, 11, 13])
+    end
+
+    it "transforms input into an instance of CalculatorInput" do
+      interpreter = Interpreter.new
+      calculator_input = interpreter.interpret('10 + 4 - 3')
+      expect(calculator_input).to be_a CalculatorInput
     end
 
   end
