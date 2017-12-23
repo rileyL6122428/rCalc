8a46f76766296c1ab38f89c1bcc6e01116247be2
Rowan Littlefield <rowanlittlefield@Rowans-MacBook-Pro.local>
2017-12-22 20:01:00 -0500

    Interpreter#interpret returns CalculatorInput

--- src/interpreter.rb
+++ src/interpreter.rb
@@ -7,10 +7,9 @@ class Interpreter
   def interpret(input)
     formatted_input = input.gsub(' ', '')
     numbers = formatted_input.split(/\+|-/).map(&:to_f)
-    operators = formatted_input.split(/\d+|\d+\.\d+/).map(&:to_sym)
+    operators = formatted_input.scan(/\+|-/).map(&:to_sym)
 
     initial_value = numbers.shift
-    byebug
     operation_queue = (0...numbers.length).map do |index|
       Operation.new(operator: operators[index], number: numbers[index])
     end
