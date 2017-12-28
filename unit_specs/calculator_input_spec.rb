19a9e5a5a0a98de9f32be6529867aeed3c81180f
Rowan Littlefield <rowanlittlefield@Rowans-MacBook-Pro.local>
2017-12-27 20:57:48 -0500

    adds unit spec for CalculatorInput#==

--- unit_specs/calculator_input_spec.rb
+++ unit_specs/calculator_input_spec.rb
@@ -19,7 +19,7 @@ describe CalculatorInput do
 
   it "initializes with an operation queue" do
     operations = []
-    
+
     @calculator_input.each_operation do |operation|
       operations << operation
     end
@@ -29,6 +29,68 @@ describe CalculatorInput do
     expect(operations[0].number).to eq(3)
     expect(operations[1].operator).to be(:-)
     expect(operations[1].number).to eq(5)
+  end
+
+  describe "#==" do
+    it "returns true when the initial value and operation queue are equivalent" do
+      same_input = CalculatorInput.new(
+        initial_value: 6,
+        operation_queue: [
+          Operation.new(operator: :+, number: 3),
+          Operation.new(operator: :-, number: 5),
+        ]
+      )
+      expect(@calculator_input == same_input).to be(true)
+    end
+
+    it "returns false when initial values differ" do
+      differing_input = CalculatorInput.new(
+        initial_value: 5,
+        operation_queue: [
+          Operation.new(operator: :+, number: 3),
+          Operation.new(operator: :-, number: 5),
+        ]
+      )
+
+      expect(@calculator_input == differing_input).to be(false)
+    end
+
+    it "returns false when the operation queue is different" do
+      differing_inputs = [
+        CalculatorInput.new(
+          initial_value: 6,
+          operation_queue: [
+            Operation.new(operator: :-, number: 5),
+            Operation.new(operator: :+, number: 3),
+          ]
+        ),
+        CalculatorInput.new(
+          initial_value: 6,
+          operation_queue: [
+            Operation.new(operator: :+, number: 18),
+            Operation.new(operator: :-, number: 11),
+          ]
+        ),
+        CalculatorInput.new(
+          initial_value: 6,
+          operation_queue: [
+            Operation.new(operator: :-, number: 3),
+            Operation.new(operator: :+, number: 5),
+          ]
+        ),
+        CalculatorInput.new(
+          initial_value: 6,
+          operation_queue: [
+            Operation.new(operator: :-, number: 5),
+            Operation.new(operator: :+, number: 3),
+            Operation.new(operator: :+, number: 10),
+          ]
+        ),
+        CalculatorInput.new(initial_value: 6, operation_queue: [])
+      ]
+      differing_inputs.each {|input| expect(@calculator_input == input).to be(false)}
+
+    end
   end
 
 end
