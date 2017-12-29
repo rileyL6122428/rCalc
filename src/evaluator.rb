31bf758131ca1025043b4b5da73bddbdb20a10f7
Rowan Littlefield <rowanlittlefield@Rowans-MacBook-Pro.local>
2017-12-28 21:42:55 -0500

    Evaluator#evaluate evaluates calculator_input

--- src/evaluator.rb
+++ src/evaluator.rb
@@ -1,7 +1,13 @@
 class Evaluator
 
-  def evaluate(numbers)
-    numbers.reduce(:+)
+  def evaluate(input)
+    result = input.initial_value
+
+    input.each_operation do |operation|
+      result = result.send(operation.operator, operation.number)
+    end
+    
+    result
   end
 
 end
