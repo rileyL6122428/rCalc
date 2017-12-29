053991b0250e8ec425837a166966110dd3136dd0
Rowan Littlefield <rowanlittlefield@Rowans-MacBook-Pro.local>
2017-12-28 21:35:39 -0500

    updates Evaluator unit specs

--- unit_specs/evaluator_spec.rb
+++ unit_specs/evaluator_spec.rb
@@ -1,14 +1,25 @@
 require 'rspec'
 require_relative '../src/evaluator.rb'
+require_relative '../src/calculator_input.rb'
 
 describe 'Evaluator' do
 
   describe '#evaluate' do
 
-    it "sums an array of numbers" do
+    it "evaluates the operation queue of a calculator input" do
       evaluator = Evaluator.new
-      expect(evaluator.evaluate([1, 2, 3])).to eq(6)
+      input = CalculatorInput.new(
+        initial_value: 10,
+        operation_queue: [
+          Operation.new(operator: :+, number: 4),
+          Operation.new(operator: :-, number: 3),
+        ]
+      )
+
+      result = evaluator.evaluate(input)
+
+      expect(result).to eq(11)
     end
-    
+
   end
 end
